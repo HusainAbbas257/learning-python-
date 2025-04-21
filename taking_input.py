3ad68e834028c309d5c689dda7cbb04d1ec70206
HusainAbbas257 <sayedhusainabbasrizvi@gmail.com>
2025-04-21 13:02:48 -0700

    inputs in python are discossed

---
+++ taking_input.py
@@ -0,0 +1,7 @@
+a=input("enter number a  ")#inputs value into number a 
+b=input("enter number b  ")#inputs value into number b
+c=a+b#sum of values of a and b
+
+print("sum=",c)#doesnt give a correcct answer as values are string 
+s=int(a)+int(b)
+print("sum=",s)#now it works
