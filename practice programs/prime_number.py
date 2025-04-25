ff28a572fbc693bb19cf991302411aa01f9fe82f
HusainAbbas257 <sayedhusainabbasrizvi@gmail.com>
2025-04-25 04:38:35 -0700

    commit-mmade a program to find prime numbers and check even odd

---
+++ practice programs/prime_number.py
@@ -0,0 +1,11 @@
+num=int(input("enter a number for prime check:"))
+is_prime=True
+for i in range(2,num):
+    if(num%i==0):
+        is_prime=False
+        break
+    
+if(is_prime==True):
+    print(f"{num} is prime")
+else:
+    print(f"{num} is not prime")
