e02f66e52a307020adec2bbadc7e149aabdd1fc6
HusainAbbas257 <sayedhusainabbasrizvi@gmail.com>
2025-04-30 04:43:57 -0700

    made a simple rock paper scissor game but opponent cannot think on its own

---
+++ rock paper scissor/main.py
@@ -0,0 +1,36 @@
+# step 1.
+# prepring a system for taking proper inputs in proper variables
+player_move=input("enter you choice(rock/paper/scissor):")
+comp_move="rock"
+WINNER='player'
+if(player_move=='rock'):
+    if(comp_move=='rock'):
+        winner="tie"
+    elif(comp_move=='paper'):
+        winner="computer"
+    elif(comp_move=='scissor'):
+        winner="player"
+    else:
+        print("something went wrong")
+elif(player_move=="paper"):
+    if(comp_move=='rock'):
+        winner="player"
+    elif(comp_move=='paper'):
+        winner="tie"
+    elif(comp_move=='scissor'):
+        winner="computer"
+    else:
+        print("something went wrong")
+elif(player_move=="scissor"):
+    if(comp_move=='rock'):
+        winner="computer"
+    elif(comp_move=='paper'):
+        winner="player"
+    elif(comp_move=='scissor'):
+        winner="tie"
+    else:
+        print("something went wrong")
+else:
+    print("something went wrong")
+
+print(f"winner of this match is:{winner}")
